70967605c32e619d88367514ebec6205eb661f9b
ymin46500-alt <ymin46500@gmail.com>
2026-07-18 11:03:24 -0500

    Add files via upload

---
+++ SEMANA 3 APOLINARIO BENDEZU SALMA.R
@@ -0,0 +1,16 @@
+#SEMANA 3
+
+#DATOS (numero de casos)
+asma<-c(18,30,39,47,11)
+covid<-c(35,23,12,10,9)
+herpes<-c(11,29,30,32,20)
+
+#CALCULAR EL PROMEDIO
+mean(asma)
+mean(covid)
+mean(herpes)
+
+#CALCULAR LA DESVIACION
+sd(asma)
+sd(covid)
+sd(herpes)
