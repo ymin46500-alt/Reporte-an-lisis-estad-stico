58006a55f9b1b2dff6701aa5ef09758417059425
ymin46500-alt <ymin46500@gmail.com>
2026-07-18 11:04:18 -0500

    Add files via upload

---
+++ SEMANA 4 APOLINARIO BENDEZU SALMA.R
@@ -0,0 +1,16 @@
+#SEMANA 4
+
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("tidyr")
+library(tidyr)
+
+install.packages("readr")
+library(readr)
+
+install.packages("shinydashboard")
+library(shinydashboard)
